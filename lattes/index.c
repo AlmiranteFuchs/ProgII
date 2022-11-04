d6fff4c6907975d8bd08d6e88fe570d759b8d2cc
BRUNO FUCHS SANTOS DA SILVA <bfss21@inf.ufpr.br>
2022-11-04 15:11:19 -0300

    updates

--- lattes/index.c
+++ lattes/index.c
@@ -5,98 +5,112 @@
 #include <string.h>
 #define LINESIZE 500000
 
-int menu();
+int parse_file_tag(char *filename);
+void program_params(char **cvs, char **q_conf, char **q_period, int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    // TEST
-    // Path Test
-    if (argc <= 1)
+    char *cvs = NULL;
+    char *q_conf = NULL;
+    char *q_period = NULL;
+
+    program_params(&cvs, &q_conf, &q_period, argc, argv);
+
+    // printf("flag_a = %s, flag_b = %s, value_c = %s\n", cvs, q_conf, q_period);
+    return 0;
+}
+
+// Parse the command line parameters
+void program_params(char **cvs, char **q_conf, char **q_period, int argc, char *argv[])
+{
+    int option;
+
+    opterr = 0;
+
+    while ((option = getopt(argc, argv, "d:c:p:")) != -1)
     {
-        printf("Sem argumentos especificados, especifique o caminho para diretório\n");
-        exit(0);
+        switch (option)
+        {
+        case 'd': // option -a was set
+            *cvs = optarg;
+            break;
+        case 'c': // option -b was set
+            *q_conf = optarg;
+            break;
+        case 'p': // option -c was set with value
+            *q_period = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s -a -b -c value\n", argv[0]);
+            exit(1);
+
+            parse_file_tag(argv[1]);
+        }
     }
 
+    // printf("flag_a = %s, flag_b = %s, value_c = %s\n", cvs, q_conf, q_period);
+
+    // Param test
+    if (cvs == NULL || q_conf == NULL || q_period == NULL)
+    {
+
+        fprintf(stderr, "Parâmetros insuficientes, uso: %s -d <diretorio com os CVs> -c <arquivo com a lista Qualis Conf> -p <arquivo com a lista Qualis Periódicos>\n", argv[0]);
+        exit(1);
+    }
+}
+
+// Reads file and sets to array
+char *parse_file(char *filename)
+{
     // Reading file
     FILE *file_ptr;
 
     // Read file
-    file_ptr = fopen(argv[1], "r");
+    file_ptr = fopen(filename, "r");
     if (file_ptr == NULL)
     {
         printf("Erro ao abrir o arquivo\n");
         exit(0);
     }
 
-    // Contents of File
+    // Get size of file
+    fseek(file_ptr, 0, SEEK_END);
+    long file_size = ftell(file_ptr);
+    rewind(file_ptr);
 
-    char character;
-
-    while ((character = fgetc(file_ptr)) != EOF)
+    // Allocs array of char of file_size
+    char *file_content = (char *)malloc(sizeof(char) * file_size);
+    if (file_content == NULL)
     {
-        if (character == '<')
-        {
-            char tag[99999] = "";
-            int i = 0;
-
-            tag[i] = character;
-            i++;
-
-            while (character != '>' && character != EOF)
-            {
-                character = fgetc(file_ptr);
-                tag[i] = character;
-                i++;
-            }
-            printf("%s\n\n", tag);
-        }
+        printf("Erro ao alocar memória\n");
+        exit(0);
     }
+
+    // Read file to array
+    fread(file_content, sizeof(char), file_size, file_ptr);
+
+    // Close file
     fclose(file_ptr);
-    return 1;
-    // TEST
+    return file_content;
 }
 
-int convert_utf8()
+int parse_file_tag(char *file_content)
 {
-    return 0;
-}
+    // Search substring
+    char *tag_ = "DETALHAMENTO-DO-ARTIGO";
 
-int menu()
-{
-    unsigned int menu_choice;
-    int menu_exited = 0;
-
-    printf("\n\n\t\tAnálise Currículo - Lattes Qualli\n\n\n");
-    while (!menu_exited)
+    char *tag_ptr = strstr(file_content, tag);
+    if (tag_ptr == NULL)
     {
-        printf("\n\n\t\t\tMenu Principal\n\n");
-        printf("\t\t1 - Periódicos por estrato\n");
-        printf("\t\t2 - Conferências por estrato\n");
-        printf("\t\t3 - Autoria por estrato\n");
-        printf("\t\t4 - Sair\n\n");
-        printf("\t\tDigite a opção desejada: ");
-        scanf("%d", &menu_choice);
-        switch (menu_choice)
-        {
-        case 1:
-            printf("\n\n\t\t\tNão implementado\n\n");
-            menu_exited = 1;
-            break;
-        case 2:
-            printf("\n\n\t\t\tNão implementado\n\n");
-            menu_exited = 1;
-            break;
-        case 3:
-            printf("\n\n\t\t\tNão implementado\n\n");
-            menu_exited = 1;
-            break;
-        case 4:
-            printf("\n\n\t\t\tNão implementado\n\n");
-            menu_exited = 1;
-        default:
-            printf("\n\n\t\t\tOpcção Inválida\n\n");
-            break;
-        }
+        printf("Não foi encontrado a tag\n");
+        exit(0);
     }
+
+    // Offsets file_content after tag
+    tag_ptr = tag_ptr + strlen(tag);
+
+    // Print content
+    printf("%s\n", tag_ptr);
+
     return 0;
 }
